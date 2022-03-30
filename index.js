1365fed8f20aafd60a7350127d6fa10f463a2a77
Rafael Krueger <rafaelkrueger565@gmail.com>
2022-03-30 16:21:01 -0300

    first commit

--- index.js
+++ index.js
@@ -3,26 +3,23 @@ const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors")
 const bcrypt = require("bcryptjs")
-const stripe = require('stripe');
+const Camisetas = require("./models/camisetas")
 const PORT = 3004
-
-//Database
 const mongoose = require("mongoose");
-const CamisaMain = require("./camisasMain")
-const newUsuarios = require("./usuariosbd.js")
 
 //middlewares
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
 app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+    res.header("Access-Control-Allow-Origin", "*")
     app.use(cors())
     next()
 })
 
+
 //Database connection
-mongoose.connect('mongodb://localhost/produtos',
+mongoose.connect("mongodb+srv://rafaelkrueger:Vidanormal01@social-soccer.r5vh4.mongodb.net/social-soccer?retryWrites=true&w=majority",
 {useNewUrlParser: true, 
     useUnifiedTopology: true}).then(()=>{
     console.log("Banco conectado!")
@@ -30,140 +27,73 @@ mongoose.connect('mongodb://localhost/produtos',
     console.log(err)
 })
 
-//Routa de camisas Main
+//promisses
+const sleep = (ms) =>{
+    return new Promise((resolve) => setTimeout(resolve,ms))
+}
 
-app.get('/produtos-main', (req, res)=>{
-       
-    CamisaMain.find().then((produtos)=>{
-        res.send(produtos)
-    }).catch((err)=>{
-        console.log(err)
-    })
+/*const newCamisa = new Camisetas({
+    name:"Paris Saint German",
+    description:"dasjdlaskjdslkajld",
+    image:"daskdçaskdçasdk",
+    value:55,
+    size:"M"
 })
+newCamisa.save((err, camisa)=>{
+    if(err) console.log(err)
+    console.log(camisa)
+})*/
 
-app.get('/produtos-main-singlepage/:_id', (req, res)=>{
-       
-    CamisaMain.findOne({_id:req.params._id})
-    .then((produtos)=>{
-        console.log(produtos)
-        res.send(produtos)
-    }).catch((err)=>{
+/*Camisetas.findOneAndDelete({name:"dsads"},(err)=>{
+    if(err){
         console.log(err)
-    })
-})
+    }
+    console.log("Deleted")
+})*/
 
-app.get('/search-produtos-main/:time', (req, res)=>{
-       
-    CamisaMain.find({time:req.time.params}).then((produtos)=>{
-        res.send(produtos)
-    }).catch((err)=>{
-        console.log(err)
-    })
-})
 
-app.post('/new-camisa-main', ((req, res)=>{
-    console.log("Acessado!")
-    new CamisaMain({
-            time:req.body.time,
-            descricao:req.body.descricao,
-            preco:req.body.preco,
-            tamanhos:"P M G GG",
-            image:req.body.image 
-    }).save()
-        .then((response)=>{
-            console.log("camisa inserida " + response)
+app.get("/camisetas", async (req,res)=>{
+
+    const main = async () =>{
+        await sleep(15000)
+        const FindCamisa = await Camisetas.find().exec((err,camisa)=>{
+            if(err)console.log(err)
+            res.send(camisa)
         })
-            .catch((err)=>{console.log(err)})
+    }
+    main();        
     
-}))
-
-app.post("/delete-camisa-main", (req, res)=>{
-    CamisaMain.deleteOne({_id:req.body._id})
-   .then((deletados)=>{
-       console.log(deletados)
-   }).catch((err)=>{
-       console.log(err)
-   }) 
 })
 
-//Rota de inserir usuario
+app.get("/camisetas-search", async (req,res)=>{
 
-app.get('/lista-usuarios', (req, res)=>{
-    newUsuarios.find()
-    .then((user)=>{
-        res.send(user);
-    })
-    .catch((err)=>{
-        console.log(err);
-    })
-
-})
-
-
-
-app.post('/inserir-usuario', (req, res)=>{
+    const main = async () =>{
+        await sleep(15000)
+        const FindCamisa = await Camisetas.findAll().exec((err,camisa)=>{
+            if(err)console.log(err)
+            res.send(camisa)
+        })
+    }
+    main();        
     
-    new newUsuarios({
-        email:req.body.newUsuario,
-        password:req.body.password,
-        adress:" ",
-        number:req.body.number,
-        CPF:req.body.CPF, 
-    }).save()
-    .then((response)=>{
-        console.log("Novo usuario inserido! " + response)
-    })
-    .catch((err)=>{console.log(err)})
 })
 
-app.post("/delete-user", (req, res)=>{
-    newUsuarios.deleteOne({_id:req.body._id})
-   .then((deletados)=>{
-       console.log(deletados)
-   }).catch((err)=>{
-       console.log(err)
-   }) 
-})
 
-app.post("/login-usuario", (req, res)=>{
-
-    newUsuarios.find({email:req.body.usuario, password:req.body.password})
-    .then((user)=>{
-        if(console.log(user[0].isAdmin == 0)){
-            console.log("Direcionar para Carrinho")    
-        }else{
-            console.log("Direcionar para ADMIN")
-        }
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
+app.get("/camisetas/:_id", async (req, res)=>{
+    const main = async () =>{
+        await sleep(15000)
+        const FindCamisa = await Camisetas.findOne({_id:req.params}).exec((err,camisa)=>{
+            if(err)console.log(err)
+            res.send(camisa)
+        })
+    }
+    main();        
 
 })
 
-app.get('/purchase', (req,res)=>{
-    const session = stripe.checkout.sessions.create({
-        payment_method_types: ['cards'],
-        line_items:[
-            {
-                price_data:{
-                    currency:'brl',
-                    product_data:{
-                        name:'Premium Encurtador de Link',
-                    },
-                    unit_amount:2000,
-                },
-                quantity:1,
-            },
-        ],
-        mode:'payment',
-        success_url:'http://localhost:3000/purchase',
-        cancel_url:'http://localhost:3000'
-    });
-    res.json({url:session.url})
-})
+
 
 //Door
-app.listen(process.env.PORT || PORT,()=>{
+app.listen(8080,()=>{
     console.log("Running")
 })
