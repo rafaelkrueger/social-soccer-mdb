cfb844014cfe49e1e6a8ca589e2b6ed83e3448b4
Rafael Krueger <rafaelkrueger565@gmail.com>
2022-05-30 10:35:45 -0300

    cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
 app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin", "https://social-soccer.netlify.app/")
     app.use(cors())
     next()
 })
